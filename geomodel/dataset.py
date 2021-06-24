9d11286eda1751b1d5cc199b65a163cd8f2b5593
Brett Byrnes <brettbyrnes@gmail.com>
2021-06-24 16:22:49 -0400

    model, feature, data code

--- geomodel/dataset.py
+++ geomodel/dataset.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import torch
@@ -5,60 +6,26 @@ from torch.utils.data import Dataset
 
 class geoDataSet(Dataset):
 
-    def __init__(self, labels_file, features_file):
+    def __init__(self, data_file):
         
         super(geoDataSet).__init__()
 
-        self.labels = np.load(labels_file)
-        self.features = np.load(features_file)
-        self.data = pd.DataFrame({"labels": self.labels, "1": self.features[:,0], "2": self.features[:,1], "3": self.features[:,2]})
-        self.label_count = self.labels.shape[0]
-        self.context_size = self.features.shape[1]
+        data = np.load(data_file)
 
+        self.X = torch.from_numpy(data[:,1:])
+        self.Y = torch.from_numpy(data[:,0])
+        self.len = len(self.Y)
+        
 
     def __len__(self):
 
-        return len(self.labels)
+        return self.len
 
 
     def __getitem__(self, idx):
 
-        if torch.is_tensor(idx):
-            idx = torch.Tensor.tolist(idx)
+        X = self.X[idx]
+        Y = self.Y[idx]
 
-        X = self.data.iloc[idx,1:]
-        y = self.data.iloc[idx,0]
+        return {"context": X, "target": Y}
 
-        X = torch.from_numpy(X.to_numpy())
-        y = torch.from_numpy(np.array(y))
-
-        return {"context": X, "target": y}
-
-
-class geoDataSet(Dataset):
-
-
-    def __init__(self, labels_file, features_file):
-        
-        super(geoDataSet).__init__()
-
-        self.labels = torch.from_numpy(np.load(labels_file))
-        self.features = torch.from_numpy(np.load(features_file))
-        self.label_count = len(torch.unique(self.labels))
-        self.context_size = self.features.shape[1]
-
-
-    def __len__(self):
-
-        return len(self.labels)
-
-
-    def __getitem__(self, idx):
-
-        if torch.is_tensor(idx):
-            idx = idx.to_list()
-
-        X = self.features[idx]
-        y = self.labels[idx]
-
-        return {"context": X, "target": y}
--- geomodel/models.py
+++ geomodel/models.py
@@ -11,20 +11,23 @@ class geoModel(nn.Module):
         super(geoModel, self).__init__()
         
         self.embeddingLayer = nn.Embedding(number_of_points, embedding_dimension)
+        print(self.embeddingLayer)
         self.linear1 = nn.Linear(feature_size*embedding_dimension, intermediate_size)
+        print(self.linear1)
         self.linear2 = nn.Linear(intermediate_size, number_of_points)
   
 
     def forward(self, inputs):
-        
+
         if inputs.dim() > 1:
-            input_rows = inputs.shape[0]
+            input_rows = inputs.shape[inputs.dim()-2]
         else:
             input_rows = 1
 
         embeddings = self.embeddingLayer(inputs).view([input_rows,-1])
         output = F.relu(self.linear1(embeddings))
         log_probs = F.log_softmax(self.linear2(output),dim=-1)
+
         return log_probs
 
 
